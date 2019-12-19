c53e049836e0219b1e3b17277efa5d13492e73ca
Max Courneya <max@mlcgaming.com>
2019-12-19 08:48:16 -0600

    Removed unnecessary libraries from classes

---
+++ BuddyPals/Versioning/VersionManifest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BuddyPals.Versioning
+{
+    public class VersionManifest
+    {
+        public List<VersionPackage> Versions { get; protected set; }
+
+        public VersionManifest(params VersionPackage[] versions)
+        {
+            Versions = new List<VersionPackage>();
+            foreach(VersionPackage version in versions)
+            {
+                Versions.Add(version);
+            }
+        }
+
+        [JsonConstructor]
+        public VersionManifest(List<VersionPackage> versions)
+        {
+            Versions = new List<VersionPackage>(versions);
+        }
+    }
+}
---
+++ BuddyPals/Versioning/VersionPackage.cs
@@ -0,0 +1,18 @@
+﻿namespace BuddyPals.Versioning
+{
+    public class VersionPackage
+    {
+        public string DisplayID { get; private set; }
+        public string VersionName { get; protected set; }
+        public ForgePackage ForgeRequirement { get; protected set; }
+        public string DownloadURL { get; protected set; }
+
+        public VersionPackage(string displayID, string versionName, ForgePackage forgeRequirement, string downloadURL)
+        {
+            DisplayID = displayID;
+            VersionName = versionName;
+            ForgeRequirement = forgeRequirement;
+            DownloadURL = downloadURL;
+        }
+    }
+}
